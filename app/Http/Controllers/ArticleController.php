27ea87253360c6360ba3e70875c0a6e926ef1958
Karim <karim.ayoub.job@gmail.com>
2025-12-15 16:51:29 +0100

    Ajout ArticleControlleur

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function post($id = 1): string
+    {
+        return " Article portant l’identifiant $id.";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ArticleController;
 
 Route::get('/',  [IndexController::class, 'index']);
+
+Route::get('/article/{id}', [ArticleController::class, 'post']);
