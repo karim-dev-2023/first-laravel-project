4567a0520dfa9f2a32c80d3a625d8ba7b66acfa6
Karim <karim.ayoub.job@gmail.com>
2025-12-16 12:19:35 +0100

    Ajout d'un tableau dans indexcontroller

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,10 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
+
 class IndexController extends Controller
 {
     public function index()
     {
-       return view('welcome', ["name" => "Rimk"]);
+        $articles = [
+            [
+                'titre' => 'L’IA soigne mieux',
+                'description' => 'L’intelligence artificielle aide les médecins à diagnostiquer plus vite.',
+            ],
+            [
+                'titre' => 'Villes vertes',
+                'description' => 'Les métropoles deviennent plus écologiques et durables',
+            ],
+            [
+                'titre' => 'Télétravail',
+                'description' => 'Plus de liberté, mais aussi plus de solitude.',
+            ],
+        ];
+    
+        return view('welcome', ["name" => 'rimk',"articles" => $articles]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,8 @@
 @section('title', 'Accueil')
 @section('content')
 <h2>Bienvenue sur le site de {{$name}}</h2>
-<x-article titre="L’IA soigne mieux" description="L’intelligence artificielle aide les médecins à diagnostiquer plus vite.">
-</x-article>
-<x-article titre="Villes vertes" description="Les métropoles deviennent plus écologiques et durables">
-</x-article>
-<x-article titre="Télétravail" description="Plus de liberté, mais aussi plus de solitude.">
-</x-article>
+@foreach ($articles as $article)
+<x-article :titre="$article['titre']" :description="$article['description']" />
+@endforeach
+
 @endsection
