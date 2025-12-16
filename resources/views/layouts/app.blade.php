5f4b2c4f80e809e5324d703630630f41d9cb50af
Karim <karim.ayoub.job@gmail.com>
2025-12-16 15:05:13 +0100

    Importation du CSS et js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <title>@yield('title')</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
