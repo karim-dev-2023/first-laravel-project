f8d239f7cfa6104cbdf3990f6bc7af4e887aef60
Karim <karim.ayoub.job@gmail.com>
2025-12-16 14:33:12 +0100

    Correction pour ne pas afficher le dernier élément

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,10 @@
 @section('content')
     <h2>Bienvenue sur le site de {{ $name }}</h2>
     @foreach ($articles as $article)
-        <x-article :titre="$article['titre']" :description="$article['description']" />
-
         @if ($loop->last)
             @break
         @endif
+        <x-article :titre="$article['titre']" :description="$article['description']" />
     @endforeach
 
 @endsection
