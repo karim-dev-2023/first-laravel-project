074dfe049859ad088cc8b3751b619a2cac1d4a38
Karim <karim.ayoub.job@gmail.com>
2025-12-16 14:27:58 +0100

    Arret de la boucle à la dernière itérations

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,13 @@
 @extends('layouts.app')
 @section('title', 'Accueil')
 @section('content')
-<h2>Bienvenue sur le site de {{$name}}</h2>
-@foreach ($articles as $article)
-<x-article :titre="$article['titre']" :description="$article['description']" />
-@endforeach
+    <h2>Bienvenue sur le site de {{ $name }}</h2>
+    @foreach ($articles as $article)
+        <x-article :titre="$article['titre']" :description="$article['description']" />
+
+        @if ($loop->last)
+            @break
+        @endif
+    @endforeach
 
 @endsection
